44b51451ed8e95ef918b59e9b997a1e776979af5
Quinn O'Neill <quinnoneill@Quinns-MacBook-Pro-2.local>
2017-12-13 19:21:11 -0700

    controller file is beefed up

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -5,3 +5,21 @@ var mongoDB = 'mongodb://127.0.0.1/shelters';
 mongoose.connect(mongoDB, {
   useMongoClient: true
 });
+
+let Weather = require('../models/weather');
+
+function displayWeather(req, response) {
+	// console.log(req.token);
+	let options = {
+		url: "http://api.wunderground.com/api/e1d4afd4ba6ea0d5/conditions/q/CO/Denver.json",
+		headers: {Authorization: "Bearer " + req.token}
+	};
+	request(options, function(err, res ,body) {
+		if(err) {console.log("ERROR: " + err);}
+		let bod = JSON.parse(body);
+		let weather = bod.businesses;
+		response.render('page', { weather });
+	});
+}
+
+module.exports = displayWeather;
