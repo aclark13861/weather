52961f6059f9057baee98f1a14f105758ca442a7
Quinn O'Neill <quinnoneill@Quinns-MacBook-Pro-2.local>
2017-12-31 19:03:50 -0700

    different files

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,45 @@
-var form = $('#newCommentForm');
+$(document).ready(function() {
 
- 	$("form").on("submit", function(event) {
-         event.preventDefault();
-          
-         var formData = $(this).serialize();
-          console.log(formData);
-          console.log($(this).attr("data-id"));
-          
+	$('#delete1').click(function() {
+		$(this).parent().remove();
+	});
 
-          console.log($("input[name=title]").val());
+	$('#delete2').click(function() {
+		$(this).parent().remove();
+	});
 
-       $.post('/commentGrabber/', formData).done(function(responseData) {
-            console.log(responseData);
-       });
-    });     
+	$('#delete3').click(function() {
+		$(this).parent().remove();
+	});
+
+	$('#delete4').click(function() {
+		$(this).parent().remove();
+	});
+
+
+	var form = $('#newCommentForm');
+
+	 	$(form).on("submit", function(event) {
+	         event.preventDefault();
+	          
+	         var formData = $(this).serialize();
+	          console.log(formData);
+	          console.log($(this).attr("data-id"));
+	          
+
+	          console.log($("input[name=title]").val());
+	        
+	          var comment = $("input[name=title]").val();
+	        $.ajax({
+	        	type: "POST",
+	        	url: "/commentGrabber",
+	        	data: String,
+	        	success: function(){$(this).dialog( "close" );},
+
+	        });
+
+	        return false;  
+
+
+	    });
+});
