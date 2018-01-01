1674d3160d8316dd802bc8dfaa4720277a2859ba
Quinn O'Neill <quinnoneill@Quinns-MacBook-Pro-2.local>
2017-12-31 21:53:13 -0700

    testing middle ware

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -23,7 +23,7 @@ function displayWeather(req, response) {
 }
 
 function saveComments(req,response) {
-	Weather.find(req.params.id, function(err, weather) {
+	Weather.find(req.body, function(err, weather) {
 		let newComment = req.body;
 		console.log(req.body);
 
